24f912a61346340b270c27c2d1af83d9d773273e
Nithish D <48820593+nithish1245@users.noreply.github.com>
2021-08-09 19:23:26 +0530

    Delete ExpenseTracker directory

--- ExpenseTracker/src/main/java/com/expense/tracker/ExpenseRepository.java
+++
@@ -1,8 +0,0 @@
-package com.expense.tracker;
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface ExpenseRepository extends JpaRepository<Expense, String> {
-	Expense findByUniid(String uniid);
-}
--- ExpenseTracker/src/main/java/com/expense/tracker/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package com.expense.tracker;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface UserRepository extends JpaRepository<User, String>{
-   User findByEmail(String email);
-}
